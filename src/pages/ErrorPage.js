0bd3f1ab5a95d87bff5f5ca5aa8ae171a968cdb1
Zidane Innis <zidaneinnis@gmail.com>
2023-01-19 17:28:32 -0500

    Error Pages and NavLinks

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainNavigation from "../components/MainNavigation";
+
+function ErrorPage() {
+  return (
+    <div>
+      <MainNavigation />
+      <h1>We encountered an error! Page not found.</h1>
+    </div>
+  );
+}
+
+export default ErrorPage;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import MainNavigation from "../components/MainNavigation";
 
 function HomePage() {
   return (
